a684fb5bd061b731cf5da0e2a1efd7533746ee8c
Shubham Vishwakarma <subhamvishwakarma32@gmail.com>
2024-08-10 21:16:07 +0530

    first commit

---
+++ src/test.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   const toggleButton = document.getElementById('toggleButton');
+//   const additionalBoxes = document.querySelector('.additional-boxes');
+
+//   toggleButton.addEventListener('click', function() {
+//       if (additionalBoxes.classList.contains('hidden')) {
+//           additionalBoxes.classList.remove('hidden');
+//           toggleButton.textContent = 'Less';
+//       } else {
+//           additionalBoxes.classList.add('hidden');
+//           toggleButton.textContent = 'More';
+//       }
+//   });
+// });
